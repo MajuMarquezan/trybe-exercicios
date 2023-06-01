8ad3b7987bbc1f0d15798a55c76485bb60e69fdf
Maju Marquezan <majumarquezan@hotmail.com>
2023-06-01 18:23:29 -0300

    finaliza exemplo

--- _fundamentos/periodo-1/3-introducao-javascript/array-e-estrutura-de-repeticao/exemplorepeticao.js
+++ _fundamentos/periodo-1/3-introducao-javascript/array-e-estrutura-de-repeticao/exemplorepeticao.js
@@ -9,3 +9,12 @@ console.log('Acabou a corrida!');
 let index = 1;
 
 index += 1;
+
+//////*********************outro exemplo */
+
+const shopping = ['Sabão em pó', 'Macarrão', 'Leite Condensado', 'Refrigerante', 'Maçã', 'Alface', 'Nescau']
+
+for (let index = 0; index < shopping.length; index += 1) {
+    // console.log(index);
+    console.log(shopping[index]);
+}
