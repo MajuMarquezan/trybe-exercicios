8f280ddb7db8790d00361dc97daa0596739351ab
Maju Marquezan <majumarquezan@hotmail.com>
2023-06-01 15:10:57 -0300

    exercicio exemplo array

---
+++ _fundamentos/periodo-1/3-introducao-javascript/array-e-estrutura-de-repeticao/exemplo.js
@@ -0,0 +1,23 @@
+const shoppingList = ['Arroz', 'Batata', 'Leite Ninho', 'Sucrilhos'];
+
+console.log(shoppingList[0]);
+
+shoppingList[1] = 'Suco de Cajú';
+
+console.log(shoppingList);
+
+shoppingList.pop();
+
+console.log(shoppingList);
+
+shoppingList.shift();
+
+console.log(shoppingList);
+
+shoppingList.push('Cotonetes');
+
+console.log(shoppingList);
+
+shoppingList.unshift('Água de Coco');
+
+console.log(shoppingList);
