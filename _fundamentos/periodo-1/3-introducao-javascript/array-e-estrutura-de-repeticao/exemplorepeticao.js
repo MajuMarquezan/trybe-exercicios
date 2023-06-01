bacb2cade25d9f4ef022887f53f202d75e6c1a97
Maju Marquezan <majumarquezan@hotmail.com>
2023-06-01 16:09:31 -0300

    adiciona exemplo repeticao

---
+++ _fundamentos/periodo-1/3-introducao-javascript/array-e-estrutura-de-repeticao/exemplorepeticao.js
@@ -0,0 +1,11 @@
+const laps = 15;
+
+for (let index = 1; index <= laps; index += 1) {
+  console.log(`Volta ${index} completada!`);
+}
+
+console.log('Acabou a corrida!');
+
+let index = 1;
+
+index += 1;
