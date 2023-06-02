aafdbb29179b6233f2e7ee8594aa0e096db7849f
Maju Marquezan <majumarquezan@hotmail.com>
2023-06-02 13:52:13 -0300

    fazendo exercicio 1

---
+++ _fundamentos/periodo-1/3-introducao-javascript/array-e-estrutura-de-repeticao/exercicio1.js
@@ -0,0 +1,9 @@
+const numArray = [1, 2, 3]
+let varIndex;
+let valorArray;
+
+for (let index = 0; index < numArray.length; index += 1) {
+    if (numArray[index] === valorArray) {
+        varIndex = index;
+    }
+}
