741d263a1114ba650747e3cae4da0892d9a0664b
Maju Marquezan <majumarquezan@hotmail.com>
2023-07-11 08:50:41 -0300

    exercicio exemplo de forEach

--- _fundamentos/periodo-2/3-Higher-Order-Functions/script.js
+++ _fundamentos/periodo-2/3-Higher-Order-Functions/script.js
@@ -5,3 +5,9 @@ pessoasAprovadas.forEach((nome, index) => {
 });
 
 console.log(pessoasAprovadas)
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers.forEach((element) => {
+  console.log(element * 2);
+});
