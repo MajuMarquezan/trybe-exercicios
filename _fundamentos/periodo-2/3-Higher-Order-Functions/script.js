1635db2f8a04fd2eec7028c0f7b04b1a24cf3f57
Maju Marquezan <majumarquezan@hotmail.com>
2023-07-11 08:33:35 -0300

    nova seção

---
+++ _fundamentos/periodo-2/3-Higher-Order-Functions/script.js
@@ -0,0 +1,7 @@
+const pessoasAprovadas = ['Ana Beatriz', 'Caio Nunes', 'Afonso Ribeiro', 'Leonardo Lins']; // lista de nomes
+
+pessoasAprovadas.forEach((nome, index) => {
+  pessoasAprovadas[index] = pessoasAprovadas[index].toUpperCase(); // acessa cada elemento do array e atualiza para letra maiúscula
+});
+
+console.log(pessoasAprovadas)
