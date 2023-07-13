c10ffff12bab35eb81cf85362c6f8441159770f3
Maju Marquezan <majumarquezan@hotmail.com>
2023-07-13 14:28:40 -0300

    adiciona pasta spread

---
+++ _fundamentos/periodo-2/3-Higher-Order-Functions/spread-op-rest-parameters-e-object-destructuring/spread operator/script.js
@@ -0,0 +1,9 @@
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['morango', 'manga', 'banana'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['kiwi', 'laranja', 'mamão'];
+
+const fruitSalad = [...specialFruit, ...additionalItems];
+
+console.log(fruitSalad);
