1b5223e3d5d27a61e02f4828bba8d3a3132a39d9
Maju Marquezan <majumarquezan@hotmail.com>
2023-07-13 14:31:15 -0300

    adiciona pratica

--- _fundamentos/periodo-2/3-Higher-Order-Functions/spread-op-rest-parameters-e-object-destructuring/spread operator/script.js
+++ _fundamentos/periodo-2/3-Higher-Order-Functions/spread-op-rest-parameters-e-object-destructuring/spread operator/script.js
@@ -4,6 +4,9 @@ const specialFruit = ['morango', 'manga', 'banana'];
 // Faça uma lista de complementos que você gostaria de adicionar
 const additionalItens = ['kiwi', 'laranja', 'mamão'];
 
-const fruitSalad = [...specialFruit, ...additionalItems];
+const fruitSalad = (fruit, additional) => {
+    const fruitSaladaWithAdditional = [...fruit, ...additional];
+    return fruitSaladaWithAdditional;
+};
 
-console.log(fruitSalad);
+console.log(fruitSalad(specialFruit, additionalItens));
